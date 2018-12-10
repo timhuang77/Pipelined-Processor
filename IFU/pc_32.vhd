418de748f50d6510b13fc11bd1e053eac3d3fe10
richardtherich <31825060+richardtherich@users.noreply.github.com>
2018-12-10 16:21:57 -0600

    Update pc_32.vhd

--- IFU/pc_32.vhd
+++ IFU/pc_32.vhd
@@ -6,16 +6,30 @@ use work.eecs361.all;
 entity pc_32 is
   port (
 	clk	: in  std_logic;
+	pc_enable : in std_logic;
 	d	: in  std_logic_vector(31 downto 0);
 	q  	: out std_logic_vector(31 downto 0)
   );
 end pc_32;
 
 architecture structural of pc_32 is
+
+component dffr_a is
+  port (
+	clk	   : in  std_logic;
+    arst   : in  std_logic;
+    aload  : in  std_logic;
+    adata  : in  std_logic;
+	d	   : in  std_logic;
+    enable : in  std_logic;
+	q	   : out std_logic
+  );
+end component;
+
 begin
 	
-	generate_dffs : for i in 0 to 31 generate
-		dffs : dffr port map(clk, d(i), q(i));
+	generate_dffras : for i in 0 to 31 generate
+		dffras : dffr_a port map(clk, arst, '0', '0', d(i), pc_enable, q(i));
 	end generate;
 	
 end structural;
