2bb0a69ac2d0d19053a17e5381ec900dea6c4ab1
allenzhong2015 <36616412+allenzhong2015@users.noreply.github.com>
2018-12-12 00:18:36 -0600

    Update mux_3_to_1_5bits.vhd

--- mux_3_to_1_5bits.vhd
+++ mux_3_to_1_5bits.vhd
@@ -6,18 +6,18 @@ use work.eecs361.all;
 entity mux_3_to_1_5bits is
 	port (
 		sel	: in std_logic_vector(1 downto 0);
-		src00, src01, src10	: in std_logic_vector(4 downto 0);
-		z	: out std_logic_vector(4 downto 0)
+		src00, src01, src10	: in std_logic_vector(5 downto 0);
+		z	: out std_logic_vector(5 downto 0)
 	);
 end entity mux_3_to_1_5bits;
 
 architecture structural of mux_3_to_1_5bits is
 
-	signal temp	: std_logic_vector(4 downto 0);
+	signal temp	: std_logic_vector(5 downto 0);
 	
 	begin
 	
-		level_1_map : mux_n generic map(5) port map (sel => sel(0), src0 => src00, src1 => src01, z => temp);
-		level_2_map : mux_n generic map(5) port map (sel => sel(1), src0 => temp, src1 => src10, z => z);
+		level_1_map : mux_n generic map(6) port map (sel => sel(0), src0 => src00, src1 => src01, z => temp);
+		level_2_map : mux_n generic map(6) port map (sel => sel(1), src0 => temp, src1 => src10, z => z);
 		
 end architecture structural;
